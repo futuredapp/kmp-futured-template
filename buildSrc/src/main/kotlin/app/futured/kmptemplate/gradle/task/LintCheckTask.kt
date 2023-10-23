621197b8ebb5cf0c8cd8b78acae7865c67bc8d05
matejsemancik <matej.semancik@gmail.com>
2023-10-23 18:55:47 +0200

    Set up ktlint & detekt, add `lintCheck` Gradle task

---
+++ buildSrc/src/main/kotlin/app/futured/kmptemplate/gradle/task/LintCheckTask.kt
@@ -0,0 +1,38 @@
+package app.futured.kmptemplate.gradle.task
+
+import app.futured.kmptemplate.gradle.configuration.ProjectSettings
+import org.gradle.api.DefaultTask
+import org.gradle.api.plugins.ExtraPropertiesExtension
+import org.gradle.kotlin.dsl.configure
+
+/**
+ * This task performs necessary static analysis checks for all relevant
+ * gradle modules (eg. subprojects) and generates reports that can be then picked up
+ * by CI.
+ */
+open class LintCheckTask : DefaultTask() {
+
+    init {
+        group = ProjectSettings.Gradle.TaskGroup
+
+        /*
+        These tasks runs for each module that has applied ktlint or detekt plugins.
+
+        The filtering is needed due to the fact that some gradle subprojects (such as :shared)
+        do not have any gradle configuration, nor source files -- acting as an "umbrella" module.
+        */
+        configure<ExtraPropertiesExtension> {
+            project.subprojects
+                .filter { it.plugins.hasPlugin("org.jlleitschuh.gradle.ktlint") }
+                .forEach {
+                    dependsOn("${it.path}:ktlintCheck")
+                }
+
+            project.subprojects
+                .filter { it.plugins.hasPlugin("io.gitlab.arturbosch.detekt") }
+                .forEach {
+                    dependsOn("${it.path}:detekt")
+                }
+        }
+    }
+}
--- shared/feature/build.gradle.kts
+++ shared/feature/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id(libs.plugins.com.android.library.get().pluginId)
     id(libs.plugins.kotlin.multiplatform.get().pluginId)
     id(libs.plugins.kotlin.parcelize.get().pluginId)
+    id(libs.plugins.conventions.lint.get().pluginId)
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
