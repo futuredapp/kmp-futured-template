873d26da9344877d59fcd8b1782663a16fbb6f11
Rudolf Hladík <hladik.rudolf@gmail.com>
2023-11-15 15:48:37 +0100

    Update to annotations

--- shared/feature/src/commonMain/kotlin/app/futured/kmptemplate/feature/injection/FeatureModule.kt
+++ shared/feature/src/commonMain/kotlin/app/futured/kmptemplate/feature/injection/FeatureModule.kt
@@ -1,8 +1,19 @@
 package app.futured.kmptemplate.feature.injection
 
+import app.futured.kmptemplate.feature.navigation.home.HomeStackNavigator
+import app.futured.kmptemplate.feature.navigation.root.RootSlotNavigator
+import com.arkivanov.decompose.router.slot.SlotNavigation
+import com.arkivanov.decompose.router.stack.StackNavigation
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
 
 @Module
 @ComponentScan("app.futured.kmptemplate.feature")
-class FeatureModule
+class FeatureModule {
+    @Single
+    fun homeStackNavigator() = HomeStackNavigator(StackNavigation())
+
+    @Single
+    fun rootSlotNavigator() = RootSlotNavigator(SlotNavigation())
+}
