92c4d44cf415a8787f63b871fcb2a0d78aadf46a
Filip Procházka <filip.prochazka@futured.app>
2024-01-30 13:36:12 +0100

    Fix SwiftLint warnings

--- iosApp/iosApp/Views/Navigation/RootNavigationView.swift
+++ iosApp/iosApp/Views/Navigation/RootNavigationView.swift
@@ -2,15 +2,15 @@ import shared
 import SwiftUI
 
 struct RootNavigationView: View {
-    
+
     @ObservedObject @KotlinStateFlow private var slot: ChildSlot<RootDestination, RootEntry>
     private let openDeepLink: (String) -> Void
-    
+
     init(_ component: RootNavigation) {
         self._slot = .init(component.slot)
         self.openDeepLink = component.openDeepLink
     }
-    
+
     var body: some View {
         ZStack {
             if let navigationEntry = slot.child?.instance {
@@ -22,10 +22,8 @@ struct RootNavigationView: View {
                 }
             }
         }
-        .onOpenURL(
-            perform: { url in
-                openDeepLink(url.absoluteString)
-            }
-        )
+        .onOpenURL { url in
+            openDeepLink(url.absoluteString)
+        }
     }
 }
