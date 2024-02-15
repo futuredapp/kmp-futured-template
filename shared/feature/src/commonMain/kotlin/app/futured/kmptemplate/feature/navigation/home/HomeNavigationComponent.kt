dde2a10299d436f3a071b5a36083e2bd866fd53a
Pavel Měsíček <pamesicek@gmail.com>
2024-02-15 15:42:04 +0100

    Add animations

--- shared/feature/src/commonMain/kotlin/app/futured/kmptemplate/feature/navigation/home/HomeNavigationComponent.kt
+++ shared/feature/src/commonMain/kotlin/app/futured/kmptemplate/feature/navigation/home/HomeNavigationComponent.kt
@@ -3,6 +3,7 @@ package app.futured.kmptemplate.feature.navigation.home
 import com.arkivanov.decompose.Child
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.essenty.backhandler.BackHandlerOwner
 import kotlinx.coroutines.flow.StateFlow
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -10,7 +11,7 @@ import org.koin.core.component.inject
 internal class HomeNavigationComponent(
     componentContext: ComponentContext,
     args: HomeNavigationArgs
-) : HomeNavigation, HomeNavigation.Actions, ComponentContext by componentContext, KoinComponent {
+) : HomeNavigation, HomeNavigation.Actions, ComponentContext by componentContext, KoinComponent, BackHandlerOwner {
 
     private val homeNavigator: HomeStackNavigator by inject()
 
@@ -19,5 +20,9 @@ internal class HomeNavigationComponent(
 
     override val actions: HomeNavigation.Actions = this
 
+    override val backHandler = componentContext.backHandler
+
     override fun iosPopTo(newStack: List<Child<HomeDestination, HomeEntry>>) = homeNavigator.iosPop(newStack)
+
+    override fun onBack() = homeNavigator.pop()
 }
