08a7894ef193a3603fdfc21be173c85f50609f34
Pavel Měsíček <pamesicek@gmail.com>
2024-05-16 12:31:55 +0200

    Enable edgeToEdge [kmp]

---
+++ androidApp/src/main/kotlin/app/futured/kmptemplate/android/tools/Constants.kt
@@ -0,0 +1,16 @@
+@file:Suppress("MagicNumber")
+
+package app.futured.kmptemplate.android.tools
+
+import androidx.compose.animation.core.tween
+import com.arkivanov.decompose.extensions.compose.stack.animation.fade
+import com.arkivanov.decompose.extensions.compose.stack.animation.plus
+import com.arkivanov.decompose.extensions.compose.stack.animation.scale
+
+interface Constants {
+
+    object Navigation {
+        private val STACK_ANIMATION_TWEEN = tween<Float>(100)
+        val STACK_ANIMATION_CROSS_FADE_SPEC = fade(STACK_ANIMATION_TWEEN) + scale(STACK_ANIMATION_TWEEN)
+    }
+}
