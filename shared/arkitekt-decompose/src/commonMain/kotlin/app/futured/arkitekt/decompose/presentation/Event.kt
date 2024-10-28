63c6e7477ca36862108caf63f9ab119f1baddf82
matejsemancik <matej.semancik@gmail.com>
2024-10-28 20:00:19 +0100

    Remove outdated documentation

--- shared/arkitekt-decompose/src/commonMain/kotlin/app/futured/arkitekt/decompose/presentation/Event.kt
+++ shared/arkitekt-decompose/src/commonMain/kotlin/app/futured/arkitekt/decompose/presentation/Event.kt
@@ -1,7 +1,7 @@
 package app.futured.arkitekt.decompose.presentation
 
 /**
- * One time event sent from ViewModel to Component.
+ * One time event sent from Component to UI.
  *
  * Event is guaranteed to be delivered just once even if screen rotation or a similar
  * operation is in progress.
--- shared/feature-v3/src/commonMain/kotlin/app/futured/kmptemplate/feature_v3/ui/base/BaseComponents.kt
+++ shared/feature-v3/src/commonMain/kotlin/app/futured/kmptemplate/feature_v3/ui/base/BaseComponents.kt
@@ -6,18 +6,11 @@ import app.futured.arkitekt.decompose.presentation.BaseComponent
 import app.futured.arkitekt.decompose.presentation.UiEvent
 import app.futured.arkitekt.decompose.presentation.ViewState
 
-/**
- * Base screen component. Each screen in application implements [AppComponentContext] and is [NavigationActionsProducer], which means
- * it has navigation functionality.
- */
 abstract class AppComponent<VS : ViewState, E : UiEvent<VS>>(
     componentContext: AppComponentContext,
     defaultState: VS,
 ) : BaseComponent<VS, E>(componentContext, defaultState), AppComponentContext by componentContext
 
-/**
- * Base component which implements [AppComponentContext] screen component.
- */
 abstract class ScreenComponent<VS : ViewState, E : UiEvent<VS>, NAV : NavigationActions>(
     componentContext: AppComponentContext,
     defaultState: VS,
