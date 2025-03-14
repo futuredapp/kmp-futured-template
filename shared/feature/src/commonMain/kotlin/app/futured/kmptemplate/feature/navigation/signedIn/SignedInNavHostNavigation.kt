fe249be09aa811721f369afd83a51c88df06bccc
matejsemancik <matej.semancik@gmail.com>
2025-03-14 08:24:06 +0100

    Rename navigation interfaces and update navigation method names for consistency

--- shared/feature/src/commonMain/kotlin/app/futured/kmptemplate/feature/navigation/signedIn/SignedInNavHostNavigation.kt
+++ shared/feature/src/commonMain/kotlin/app/futured/kmptemplate/feature/navigation/signedIn/SignedInNavHostNavigation.kt
@@ -3,5 +3,5 @@ package app.futured.kmptemplate.feature.navigation.signedIn
 import app.futured.arkitekt.decompose.navigation.NavigationActions
 
 internal interface SignedInNavHostNavigation : NavigationActions {
-    fun SignedInNavHostComponent.toLogin()
+    fun SignedInNavHostComponent.navigateToLogin()
 }
