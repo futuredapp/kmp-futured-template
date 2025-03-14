d74667b462f7b788f268af308de7b7fc0d2aeeb8
matejsemancik <matej.semancik@gmail.com>
2025-03-14 14:25:54 +0100

    Resolve iOS build issues: Bump moko-resources lib

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,10 +1,3 @@
 plugins {
     `kotlin-dsl`
 }
-
-dependencies {
-//    implementation(libs.gradlePlugin.kotlin)
-//    implementation(libs.gradlePlugin.android)
-//    implementation(libs.gradlePlugin.ktlint)
-//    implementation(libs.gradlePlugin.detekt)
-}
