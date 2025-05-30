9c908e9c94ab3ff5170b6b6cd43c01818790cce2
matejsemancik <matej.semancik@gmail.com>
2025-05-31 00:20:09 +0200

    Use DateTimeStringDesc at FirstScreen

--- shared/feature/src/commonMain/kotlin/app/futured/kmptemplate/feature/ui/firstScreen/FirstViewState.kt
+++ shared/feature/src/commonMain/kotlin/app/futured/kmptemplate/feature/ui/firstScreen/FirstViewState.kt
@@ -3,4 +3,8 @@ package app.futured.kmptemplate.feature.ui.firstScreen
 import dev.icerock.moko.resources.desc.StringDesc
 import dev.icerock.moko.resources.desc.desc
 
-data class FirstViewState(val text: StringDesc = "".desc(), val randomPerson: StringDesc? = null)
+data class FirstViewState(
+    val createdAt: StringDesc = "".desc(),
+    val counter: StringDesc = "".desc(),
+    val randomPerson: StringDesc? = null
+)
