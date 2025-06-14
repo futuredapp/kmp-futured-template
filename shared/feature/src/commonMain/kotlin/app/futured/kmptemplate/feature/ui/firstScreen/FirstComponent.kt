5b2377c5d12baef61145bb36ededd8f24152c3f0
matejsemancik <matej.semancik@gmail.com>
2025-06-14 22:18:42 +0200

    Refactor FirstScreen for better testability

--- shared/feature/src/commonMain/kotlin/app/futured/kmptemplate/feature/ui/firstScreen/FirstComponent.kt
+++ shared/feature/src/commonMain/kotlin/app/futured/kmptemplate/feature/ui/firstScreen/FirstComponent.kt
@@ -5,14 +5,14 @@ import app.futured.kmptemplate.feature.domain.CounterUseCase
 import app.futured.kmptemplate.feature.domain.FetchDataUseCase
 import app.futured.kmptemplate.feature.ui.base.AppComponentContext
 import app.futured.kmptemplate.feature.ui.base.ScreenComponent
-import app.futured.kmptemplate.resources.MR
-import app.futured.kmptemplate.resources.datetime.desc
 import co.touchlab.kermit.Logger
 import com.arkivanov.essenty.lifecycle.doOnCreate
-import dev.icerock.moko.resources.format
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.datetime.Clock
 import org.koin.core.annotation.Factory
 import org.koin.core.annotation.InjectedParam
 import kotlin.time.Duration.Companion.milliseconds
@@ -24,6 +24,8 @@ internal class FirstComponent(
     @InjectedParam override val navigation: FirstScreenNavigation,
     private val fetchDataUseCase: FetchDataUseCase,
     private val counterUseCase: CounterUseCase,
+    override val lifecycleScope: CoroutineScope = MainScope(),
+    override val useCaseDispatcher: CoroutineDispatcher = Dispatchers.Default,
 ) : ScreenComponent<FirstViewState, FirstUiEvent, FirstScreenNavigation>(
     componentContext = componentContext,
     defaultState = FirstViewState(),
@@ -33,7 +35,7 @@ internal class FirstComponent(
     FirstScreen.Actions {
 
     companion object {
-        private const val COUNTER_ALERT_AT_SECONDS = 30L
+        private const val COUNTER_ALERT = 30L
     }
 
     private val logger = Logger.withTag("FirstComponent")
@@ -45,7 +47,6 @@ internal class FirstComponent(
         doOnCreate {
             syncData()
             observeCounter()
-            updateCreatedAtTimestamp()
         }
     }
 
@@ -53,35 +54,22 @@ internal class FirstComponent(
 
     private fun syncData() = fetchDataUseCase.execute {
         onSuccess { person ->
-            componentState.update { it.copy(randomPerson = MR.strings.first_screen_random_person.format(person.name.orEmpty())) }
+            componentState.update { it.copy(randomPerson = person) }
         }
         onError { error ->
-            componentState.update { it.copy(randomPerson = MR.strings.first_screen_random_person.format("Failed to fetch")) }
-            logger.e(error) { error.message.toString() }
+            componentState.update { it.copy(randomPersonError = error) }
         }
     }
 
     private fun observeCounter() = counterUseCase.execute(CounterUseCase.Args(interval = 200.milliseconds)) {
         onNext { count ->
-            updateCount(count)
+            logger.d { "count: $count" }
+            componentState.update { it.copy(counter = count) }
 
-            if (count == COUNTER_ALERT_AT_SECONDS) {
-                logger.d { "Counter reached 10" }
-                sendUiEvent(FirstUiEvent.ShowToast(MR.strings.first_screen_counter_alert.format(COUNTER_ALERT_AT_SECONDS)))
+            if (count == COUNTER_ALERT) {
+                logger.d { "counter reached $COUNTER_ALERT" }
+                sendEvent(FirstUiEvent.Notify(count))
             }
-        }
-        onError { error ->
-            logger.e(error) { "Counter error" }
-        }
-    }
-
-    private fun updateCount(count: Long) {
-        componentState.update { it.copy(counter = MR.strings.first_screen_counter.format(count)) }
-    }
-
-    private fun updateCreatedAtTimestamp() {
-        componentState.update {
-            it.copy(createdAt = MR.strings.first_screen_created_at.format(Clock.System.now().desc("Hms")))
         }
     }
 }
--- shared/feature/src/commonMain/kotlin/app/futured/kmptemplate/feature/ui/firstScreen/FirstViewState.kt
+++ shared/feature/src/commonMain/kotlin/app/futured/kmptemplate/feature/ui/firstScreen/FirstViewState.kt
@@ -1,6 +1,32 @@
 package app.futured.kmptemplate.feature.ui.firstScreen
 
+import app.futured.kmptemplate.network.rest.dto.Person
+import app.futured.kmptemplate.resources.MR
+import app.futured.kmptemplate.resources.datetime.desc
 import dev.icerock.moko.resources.desc.StringDesc
-import dev.icerock.moko.resources.desc.desc
+import dev.icerock.moko.resources.format
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 
-data class FirstViewState(val createdAt: StringDesc = "".desc(), val counter: StringDesc = "".desc(), val randomPerson: StringDesc? = null)
+data class FirstViewState(
+    internal val counter: Long = 0L,
+    internal val createdAt: Instant = Clock.System.now(),
+    internal val randomPerson: Person? = null,
+    internal val randomPersonError: Throwable? = null,
+) {
+    companion object {
+        fun mock() = FirstViewState()
+    }
+    val counterText: StringDesc
+        get() = MR.strings.first_screen_counter.format(counter)
+
+    val createdAtText: StringDesc
+        get() = MR.strings.first_screen_created_at.format(createdAt.desc("Hms"))
+
+    val randomPersonText: StringDesc?
+        get() = when {
+            randomPersonError != null -> MR.strings.first_screen_random_person.format("Failed to fetch")
+            randomPerson != null -> MR.strings.first_screen_random_person.format(randomPerson.name.orEmpty())
+            else -> null
+        }
+}
